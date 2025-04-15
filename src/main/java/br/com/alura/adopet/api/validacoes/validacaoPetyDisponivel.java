73dc48c7a7ee392836b6753f2ee75af99e992e18
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2025-04-14 23:09:24 -0300

    =aplicando o conceito de padrao de projeto Strategy  Chain of Responsibility

---
+++ src/main/java/br/com/alura/adopet/api/validacoes/validacaoPetyDisponivel.java
@@ -0,0 +1,23 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDTO;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.PetRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class validacaoPetyDisponivel implements ValidacaoSolicitacaoAdocao{
+    @Autowired
+    private PetRepository petRepository;
+    public void validar(SolicitacaoAdocaoDTO dto){
+        Pet pet = petRepository.getReferenceById(dto.idPet());
+
+        if (pet.getAdotado()) {
+            throw new ValidacaoException("Pet já foi adotado!");
+        }
+    }
+}
