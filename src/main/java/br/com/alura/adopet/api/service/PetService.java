fc28f0f7b8297cb2a9b9e6be9d6232ca361a9011
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2025-04-15 13:22:01 -0300

    =Ajustando controller service entidade padroes DTO e boas praticas

---
+++ src/main/java/br/com/alura/adopet/api/service/PetService.java
@@ -0,0 +1,23 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.CadastroPetDTO;
+import br.com.alura.adopet.api.dto.PetDTO;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class PetService {
+    @Autowired
+    private PetRepository repository;
+
+    public List<PetDTO> buscarPetsDisponiveis(){
+         return repository.findALLByAdotadoFalse().stream().map(PetDTO::new).toList();
+
+    }
+    public void cadastrarPet(Abrigo abrigo, CadastroPetDTO dto){
+        repository.save(new Pet());
+    }
+}
